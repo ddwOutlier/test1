1fb185dbcce943ac2fdc0e78383663b2edeee93d
ddwOutlier <87630872+ddwOutlier@users.noreply.github.com>
2021-08-20 17:33:37 +0900

    Update data.js

--- js/data.js
+++ js/data.js
@@ -2,56 +2,56 @@ const qnaList = [
   {
     q: '1. 대학원으로 올라가던 중 솜냥이와 마주쳤다. 이미 학우님들께 둘러 쌓여있는 솜냥이, 갑자기 엄청 귀여운 자세를 취한다. 이때 나는?',
     a: [
-      { answer: 'a. 아련한 눈빛을 보내며 다음을 기약한다.', type: ['shadowgam','shygam','gamja','unigam'] },
-      { answer: 'b. 미쳤나봐, 카메라를 켜고 인파 속으로 뛰어들어간다.', type: ['ssgam','burngam','unigam'] },
+      { answer: 'a. 아련한 눈빛을 보내며 다음을 기약한다.', type: ['shadowgam','shygam','gamja'] },
+      { answer: 'b. 미쳤나봐, 카메라를 켜고 인파 속으로 뛰어들어간다.', type: ['ssgam','burngam'] },
     ]
   },
   {
     q: '2. 랩실에 갔더니 동기가 울고 있다. "4시간째 코드짰는데, 갑자기 컴퓨터 전원이 꺼졌어,,"',
     a: [
-      { answer: 'a. 헉 괜찮아? 어떡해,, 저장은 했어? 급한거야? 도와줄까? 마이쮸(or 술) 먹을래?', type: ['ssgam','unigam','shygam','gamja'] },
+      { answer: 'a. 헉 괜찮아? 어떡해,, 급한거야? 마이쮸(or 술) 먹을래?', type: ['ssgam','unigam','shygam','gamja'] },
       { answer: 'b. 엥 컴퓨터가 갑자기 꺼졌다고? 낡아서 그런가,, 저장은 했어? ', type: ['burngam','shadowgam'] },
     ]
   },
     {
     q: '3. 팀플 회의 때 (그나마) 더 같이 일하고 싶은 유형은?',
     a: [
-      { answer: 'a. 그거 데이터 없을걸요? 이건 구현 힘들거 같은데,, 하나하나 실현 가능성 따지는 팀원 ', type: ['ssgam','unigam','shygam'] },
-      { answer: 'b. 오 좋아요. 헐 대박 좋은데요? 전 다 좋아요! 무슨 말을 해도 좋다고 하는 팀원', type: ['burngam','shadowgam','gamja'] },
+      { answer: 'a. 그거 데이터 없을걸요? 이건 구현 힘들거 같은데,, 하나하나 실현 가능성 따지는 팀원 ', type: ['ssgam','shadowgam','shygam'] },
+      { answer: 'b. 오 좋아요. 헐 대박 좋은데요? 전 다 좋아요! 무슨 말을 해도 좋다고 하는 팀원', type: ['burngam','unigam','gamja'] },
     ]
   },
     {
     q: '4. 프로젝트 진행 시, 당신이 주로 사용하는 것은?',
     a: [
-      { answer: 'a. 선례연구에서 많이 활용한 모델', type: ['unigam','gamja'] },
-      { answer: 'b. 발표된 지 얼마 안된 핫한 트렌드 모델', type: ['ssgam','burngam','shadowgam','shygam'] },
+      { answer: 'a. 선례연구에서 많이 활용한 모델', type: ['ssgam','unigam','gamja'] },
+      { answer: 'b. 발표된 지 얼마 안된 핫한 트렌드 모델', type: ['burngam','shadowgam','shygam'] },
     ]
   },
   {
     q: '5. 인문관 복도에서 모르는 사람이 나한테 인사한다. 이때 나는?',
     a: [
-      { answer: 'a. 나,,? 아니겠지? 못본 척 한다.', type: ['shadowgam','shygam','gamja','unigam']},
-      { answer: 'b. 아는 사람인가? 당황스럽지만 인사해준다.', type: ['ssgam','burngam','unigam']},
+      { answer: 'a. 나,,? 아니겠지? 못본 척 한다.', type: ['shadowgam','shygam','gamja']},
+      { answer: 'b. 아는 사람인가? 당황스럽지만 인사해준다.', type: ['ssgam','burngam']},
     ]
   },
    {
     q: '6. 더 선호하는 전공 강의는? (둘 다 학점은 후하다는 전제!)',
     a: [
-      { answer: 'a. "일단 들어두면 나중에 찾아서라도 쓸 수 있어요~" 배우는 건 많고 도움이 될 거 같지만 어려운 강의', type: ['burngam','shadowgam','gamja'] },
+      { answer: 'a. "일단 들어두면 나중에 찾아서라도 쓸 수 있어요~" 배우는 건 많고 도움이 될 거 같지만 어려운 강의', type: ['burngam','shadowgam','gamja','unigam'] },
       { answer: 'b. "하나를 알아도 정확하게 알아야지, 차근차근합시다" 하나하나 이해시켜줘서 좋지만 이 정도만 배워도 될지 의심스러운 강의', type: ['ssgam','unigam','shygam'] },
     ]
   },
   {
     q: '7. 팀플 진행 시에 더 짜증나는 상황은?',
     a: [
-      { answer: 'a. 8시에 시작은 회의, 7시 30분에 갑자기 회의 참석이 불가능하다고 하는 팀원', type: ['burngam','unigam','shygam','shadowgam'] },
+      { answer: 'a. 8시에 시작인 회의, 7시 30분에 갑자기 회의 참석이 불가능하다고 하는 팀원', type: ['burngam','unigam','shygam','shadowgam'] },
       { answer: 'b. 8시에 시작인 회의, 3분 늦었는데 왜 늦었냐고 뭐라 하는 팀원', type: ['ssgam','shadowgam','gamja'] },
     ]
   },
   {
     q: '8. 이 테스트를 하면서 드는 생각은?',
     a: [
-      { answer: 'a. 답안에 없는 답이 생각나면서 뭘 고를지 고민된다.', type: ['burngam','shadowgam','gamja'] },
+      { answer: 'a. 아무 생각 없다.', type: ['burngam','shadowgam','gamja'] },
       { answer: 'b. 이걸로 무슨 결과를 도출할 수 있을지 의심된다.', type: ['ssgam','unigam','shygam'] },
     ]
   },
