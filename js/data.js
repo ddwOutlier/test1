5253fc019ce3b4000ee5c08d0501c9e861916c5d
ddwOutlier <87630872+ddwOutlier@users.noreply.github.com>
2021-08-23 18:01:39 +0900

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,115 +1,176 @@
+/*
+
+*/
+
 const qnaList = [
   {
-    q: '1. 대학원으로 올라가던 중 솜냥이와 마주쳤다. 이미 학우님들께 둘러 쌓여있는 솜냥이, 갑자기 엄청 귀여운 자세를 취한다. 이때 나는?',
+    q: '1. 만석인 인문관 엘베 속 어렴풋이 아는 동기와 눈이 마주쳤다. 이때 나는?',
     a: [
-      { answer: 'a. 아련한 눈빛을 보내며 다음을 기약한다.', type: ['shadowgam','shygam','gamja'] },
-      { answer: 'b. 미쳤나봐, 카메라를 켜고 인파 속으로 뛰어들어간다.', type: ['ssgam','burngam'] },
+      { answer: 'a. 뻘줌하군,,, 간단하게 눈인사 정도 건넨다.', type: ['ISTJ','ISFJ','INFJ','INTJ','ISTP','ISFP','INFP','INTP'] },
+      { answer: 'b. ㅎㅇㅎㅇ 너도 (전공) 들으러가?', type: ['ESTJ','ESFJ','ENFJ','ENTJ','ESTP','ESFP','ENFP','ENTP'] },
     ]
   },
   {
     q: '2. 랩실에 갔더니 동기가 울고 있다. "4시간째 코드짰는데, 갑자기 컴퓨터 전원이 꺼졌어,,"',
     a: [
-      { answer: 'a. 헉 괜찮아? 어떡해,, 급한거야? 마이쮸(or 술) 먹을래?', type: ['ssgam','unigam','shygam','gamja'] },
-      { answer: 'b. 엥 컴퓨터가 갑자기 꺼졌다고? 낡아서 그런가,, 저장은 했어? ', type: ['burngam','shadowgam'] },
+      { answer: 'a. 헉 괜찮아? 어떡해,, 급한거야? 마이쮸(or 술) 먹을래?', type: ['ISFJ','INFJ','ISFP','INFP','ESFJ','ENFJ','ESFP','ENFP'] },
+      { answer: 'b. 엥 컴퓨터가 갑자기 꺼졌다고? 낡아서 그런가,, 뭐하고 있었는데? ', type: ['ISTJ','INTJ','ISTP','INTP','ESTJ','ENTJ','ESTP','ENTP'] },
     ]
   },
     {
-    q: '3. 팀플 회의 때 (그나마) 더 같이 일하고 싶은 유형은?',
+    q: '3. 프로젝트 아이디어를 나누는 시간, 둘 중 나와 더 가까운 모습은?',
     a: [
-      { answer: 'a. 그거 데이터 없을걸요? 이건 구현 힘들거 같은데,, 하나하나 실현 가능성 따지는 팀원 ', type: ['ssgam','shadowgam','shygam'] },
-      { answer: 'b. 오 좋아요. 헐 대박 좋은데요? 전 다 좋아요! 무슨 말을 해도 좋다고 하는 팀원', type: ['burngam','unigam','gamja'] },
+      { answer: 'a. 이미 나온 의견에 대하여 분석하고 질문하는 스타일', type: ['ISTJ','ISFJ','ISTP','ISFP','ESTJ','ESFJ','ESTP','ESFP'] },
+      { answer: 'b. 생각나는 아이디어를 적극적으로 던지고 보는 스타일', type: ['INFJ','INTJ','INFP','INTP','ENFJ','ENTJ','ENFP','ENTP'] },
     ]
   },
     {
     q: '4. 프로젝트 진행 시, 당신이 주로 사용하는 것은?',
     a: [
-      { answer: 'a. 선례연구에서 많이 활용한 모델', type: ['ssgam','unigam','gamja'] },
-      { answer: 'b. 발표된 지 얼마 안된 핫한 트렌드 모델', type: ['burngam','shadowgam','shygam'] },
+      { answer: 'a. 선례연구에서 많이 활용한 모델', type: ['ISTJ','ISFJ','ISTP','ISFP','ESTJ','ESFJ','ESTP','ESFP'] },
+      { answer: 'b. 발표된 지 얼마 안된 핫한 트렌드 모델', type: ['INFJ','INTJ','INFP','INTP','ENFJ','ENTJ','ENFP','ENTP'] },
     ]
   },
   {
-    q: '5. 인문관 복도에서 모르는 사람이 나한테 인사한다. 이때 나는?',
+    q: '5. 동기들과 같이 걸어가던 중, 저 멀리 교수님이 보인다. 이때 나는?',
     a: [
-      { answer: 'a. 나,,? 아니겠지? 못본 척 한다.', type: ['shadowgam','shygam','gamja']},
-      { answer: 'b. 아는 사람인가? 당황스럽지만 인사해준다.', type: ['ssgam','burngam']},
+      { answer: 'a. 다른 동기들이 인사하면 따라서 인사한다.', type: ['ISTJ','ISFJ','INFJ','INTJ','ISTP','ISFP','INFP','INTP'] },
+      { answer: 'b. 누구보다 빠르게 남들과는 다르게 먼저 인사한다.', type: ['ESTJ','ESFJ','ENFJ','ENTJ','ESTP','ESFP','ENFP','ENTP']},
     ]
   },
    {
-    q: '6. 더 선호하는 전공 강의는? (둘 다 학점은 후하다는 전제!)',
+    q: '6. 팀플 시에 더 선호하는 방식은? ',
     a: [
-      { answer: 'a. "일단 들어두면 나중에 찾아서라도 쓸 수 있어요~" 배우는 건 많고 도움이 될 거 같지만 어려운 강의', type: ['burngam','shadowgam','gamja','unigam'] },
-      { answer: 'b. "하나를 알아도 정확하게 알아야지, 차근차근합시다" 하나하나 이해시켜줘서 좋지만 이 정도만 배워도 될지 의심스러운 강의', type: ['ssgam','unigam','shygam'] },
+      { answer: 'a. 큰 틀만 잡아두고 즉흥적으로 진행하는 방식', type: ['ISTP','ISFP','ESTP','ESFP','INFP','INTP','ENFP','ENTP'] },
+      { answer: 'b. 세부적인 규칙을 정해 진행하는 방식', type: ['ISTJ','ISFJ','ESTJ','ESFJ','INFJ','INTJ','ENFJ','ENTJ'] },
     ]
   },
   {
-    q: '7. 팀플 진행 시에 더 스트레스 받는 팀원은?',
+    q: '7. 이 테스트를 하면서 드는 생각은?',
     a: [
-      { answer: 'a. 8시에 시작인 회의, 7시 30분에 갑자기 회의 참석이 불가능하다고 하는 팀원', type: ['burngam','unigam','shygam','shadowgam'] },
-      { answer: 'b. 8시에 시작인 회의, 5분 늦었는데 왜 늦었냐고 뭐라 하는 팀원', type: ['ssgam','shadowgam','gamja'] },
+      { answer: 'a. 아무 생각 없다.', type: ['ISTJ','ISFJ','ISTP','ISFP','ESTJ','ESFJ','ESTP','ESFP'] },
+      { answer: 'b. 이걸로 무슨 결과를 도출할 수 있을지 의심된다.', type:['INFJ','INTJ','INFP','INTP','ENFJ','ENTJ','ENFP','ENTP'] },
     ]
   },
   {
-    q: '8. 이 테스트를 하면서 드는 생각은?',
+    q: '8. 많은 사람들 앞에서 진행하는 프로젝트 중간 발표회, 더 받고 싶은 피드백은?',
     a: [
-      { answer: 'a. 아무 생각 없다.', type: ['burngam','shadowgam','gamja'] },
-      { answer: 'b. 이걸로 무슨 결과를 도출할 수 있을지 의심된다.', type: ['ssgam','unigam','shygam'] },
+      { answer: 'a. 학생! 열심히 했네요. 근데 이건 왜 이렇게 했습니까? 그냥 궁금해서~', type:['ISFJ','INFJ','ISFP','INFP','ESFJ','ENFJ','ESFP','ENFP'] },
+      { answer: 'b. A부분 다시 봐봐, 이거는 왜 이렇게 했어요? 이 부분 좀 잘못된 거 같은데, 다시 해봐요', type:['ISTJ','INTJ','ISTP','INTP','ESTJ','ENTJ','ESTP','ENTP']},
     ]
   },
   {
-    q: '9. 많은 사람들 앞에서 진행하는 프로젝트 중간 발표회, 더 받고 싶은 피드백은?',
+    q: '9. 교수님이 추천해주신 한 모델(방법)의 성능이 생각보다 좋지 않게 나왔다. 이때 나는?',
     a: [
-      { answer: 'a. 학생! 열심히 했네요. 근데 이건 왜 이렇게 했습니까? 그냥 궁금해서~', type: ['ssgam','unigam','gamja','shygam'] },
-      { answer: 'b. A부분 다시 봐봐, 이거는 왜 이렇게 했어요? 이 부분 좀 잘못된 거 같은데, 다시 해봐요', type: ['burngam','shadowgam'] },
+      { answer: 'a. 여러 전처리, 하이퍼파라미터 튜닝 기법을 적용해본다.(같은 방법에서 여러가지 시도해본다.)', type: ['ISTJ','ISFJ','ESTJ','ESFJ','INFJ','INTJ','ENFJ','ENTJ']},
+      { answer: 'b. 빠르게 버리고 다른 여러 모델들을 적용해본다.(다른 방법을 적용해본다.)', type: ['ISTP','ISFP','ESTP','ESFP','INFP','INTP','ENFP','ENTP'] },
     ]
   },
   {
-    q: '10. 강의실에서 나가던 중 잘 모르는 선&후배가 갑자기 나타나 인사했다. 이때 나는?',
+    q: '10. 둘 중 더 화나는 사람은?',
     a: [
-      { answer: 'a. 당황하다가 타이밍을 놓쳐서 인사를 못한다.', type: ['ssgam','gamja','shygam','shadowgam'] },
-      { answer: 'b. 안녕하세요~! ^0^ ', type: ['ssgam','unigam','burngam'] },
-      { answer: 'c. aksdf하세요. 당황해서 이상한 인사를 건넨다.', type: ['unigam','shygam','shadowgam'] },
+      { answer: 'a. 랩실 자리를 비운 사이 말도 없이 과제를 베껴 간 동기 ', type:['ISFJ','INFJ','ISFP','INFP','ESFJ','ENFJ','ESFP','ENFP'] },
+      { answer: 'b. 과제 도와준다고 깝치다가 내 과제 다 날린 동기', type:['ISTJ','INTJ','ISTP','INTP','ESTJ','ENTJ','ESTP','ENTP']},
     ]
   },
   {
-    q: '11. 오늘 만나기로 한 동기가 시간표 짜느라 잠을 설쳤다고 연락이 왔다. 이때 나의 답장은? ',
+    q: '11. 약속 시간에 늦어버린 나, 이때 내가 주로 하는 말은?',
     a: [
-      { answer: 'a. 시간표 때문에 잠을 설쳤다고..? 왜?? 그거 내일까지 수정할 수 있는데', type: ['burngam','unigam','shadowgam'] },
-      { answer: 'b. 피곤하겠다,, 너 피곤하면 그냥 다음에 볼까?', type: ['ssgam','unigam','shygam','gamja']},
+      { answer: 'a. 더운데 기다리게 해서 미안해,,, 아이스크림 사줄까? 많이 더웠지,,', type:['ISFJ','INFJ','ISFP','INFP','ESFJ','ENFJ','ESFP','ENFP'] },
+      { answer: 'b. 아니 내가 20분 전에 나왔는데, 반대로 가는 버스를 탔어 진짜 미안하다', type:['ISTJ','INTJ','ISTP','INTP','ESTJ','ENTJ','ESTP','ENTP']},
     ]
   },
   {
-    q: '12. 프로젝트 팀장을 정하는 자리 이때 나는?',
+    q: '12. 교양 시간, 시험대체로 토론을 진행하게 되었다. 이때 더 편한 것은?',
+    a:[
+      { answer: 'a. 미리 주제를 공유하고 주어진 기간동안 토론게시판에 업로드', type: ['ISTJ','ISFJ','INFJ','INTJ','ISTP','ISFP','INFP','INTP'] },
+      { answer: 'b. 당일에 주제를 알려주고 줌으로 토론 진행', type: ['ESTJ','ESFJ','ENFJ','ENTJ','ESTP','ESFP','ENFP','ENTP'] },
+  ]
+  },
+  {
+    q: '13. 알고 지내던 동기가 내 뒷담을 했다는 소식을 들었다. 이때 나는?',
     a: [
-      { answer: 'a. 팀장 하고 싶으신 분? 없으면 제가 해도 상관 없어요~ (진짜 상관 없음)', type: ['ssgam','burngam','unigam'] },
-      { answer: 'b. 가만히 있다가 아무도 나서지 않으면 그때 "그냥 제가 할게요~.."', type: ['shygam'] },
-      { answer: 'c. "전 열심히 돕겠습니다!!" 누가하든 나만 아니면 됨.', type: ['shadowgam','gamja'] },
+      { answer: 'a. 어떻게 그럴 수가 있지,, 충격이다.', type: ['ENFP','INFP','ENFJ','INFJ'] },
+      { answer: 'b. 나한테 자격지심 느끼나? 역시 나는 잘났군', type: ['ENTJ','INTJ','ENTP','INTP'] },
+      { answer: 'c. 원래 모든 사람들이 날 좋아할 수는 없는 법! (하지만 신경쓰인다,,)', type: ['ESTP','ISTP','ESFP','ISFP','ESFJ'] },
+      { answer: 'd. 나랑 친하지 않았던 동기라 상관 없음 OR 걔가 누군데', type: ['ESTJ','ISTJ','ISFJ'] },
+    ]
+  },
+  {
+    q: '14. 뭔가 구매할 때 나는?',
+    a: [
+      { answer: 'a. 0점 짜리 리뷰를 모두 읽은 뒤 신중하게 구매한다.', type: ['INTJ','ENTJ','ISTJ','ESTJ'] },
+      { answer: 'b. 친구나 지인이 사면 후기를 듣고 산다.', type: ['INFJ','ENFJ','ISFJ','ESFJ'] },
+      { answer: 'c. 귀엽거나 흥미로우면 구매한다. (충동구매)', type: ['ISFP','ISTP','ESFP','ESTP'] },
+      { answer: 'd. 과도한 생각 뒤 결국 아무것도 안 사고 산책나간다.', type: ['INFP','ENFP','INTP','ENTP'] },
     ]
   },
 ]
 
 const infoList = [
   {
-    name: '새싹 감자',
+    name: '파릇파릇 <새싹 감자> (ENFP)',
+    desc: '잘한다 잘한다하면 더 잘하는 타입! 즉흥적이고 정신없지만 미워할 수 없는 새싹 같은 파릇파릇한 매력을 지니고 있네요. 기획할 때는 신나지만 결국 싫증나서 마무리 짓지 못한 일이 오조억개입니다. 신기한 점은 그때그때 되는대로 처치 하는 것 같지만 어떻게든 성과를 내고 있다는 점! 전통적이고 수직적인 조직보다는 수평적이고 자유로운 환경에서 더 자신의 능력을 펼칠 수 있어요. '
+  },
+  {
+    name: '파릇파릇 <새싹 감자> (ESFP)',
     desc: ' 파릇파릇 '
   },
   {
-    name: '낯가리는 관종 감자',
-    desc: '낯가려서 잘 말하지 못하지만 사실 관심 받는게 즐거운 감자 '
+    name: '파릇파릇 <새싹 감자> (ESTP)',
+    desc: ' 파릇파릇 '
   },
   {
-    name: '열정열정열정! <열정 감자>',
+    name: '열정열정열정! <열정 감자> (ENTP)',
     desc: '일 벌리면 주변 사람들이 수습하고 있나요? 인생은 도박이죠'
   },
   {
-    name: '있는듯 없는듯 그림자 감자',
+    name: '열정열정열정! <열정 감자> (ENTJ)',
+    desc: '일 벌리면 주변 사람들이 수습하고 있나요? 인생은 도박이죠'
+  },
+  {
+    name: '그냥 <인간> (ENFJ)',
+    desc: 'ㅇㄹㄴ '
+  },
+  {
+    name: '그냥 <인간> (INTJ)',
+    desc: 'ㅇㄹㄴ '
+  },
+  {
+    name: '그냥 <인간> (ISTJ)',
+    desc: 'ㅇㄹㄴ '
+  },
+  {
+    name: '그냥 <인간> (ESTJ)',
+    desc: 'ㅇㄹㄴ '
+  },
+  {
+    name: '있는듯 없는듯 그림자 감자 (INTP)',
     desc: ' 그냥 인생 조용히 살고 싶어요.'
   },
   {
-    name: '말도 못하는 찐감자',
+    name: '있는듯 없는듯 그림자 감자 (ISTP)',
+    desc: ' 그냥 인생 조용히 살고 싶어요.'
+  },
+  {
+    name: '낯가리는 관종 감자 (ISFJ)',
+    desc: '낯가려서 잘 말하지 못하지만 사실 관심 받는게 즐거운 감자 '
+  },
+  {
+    name: '낯가리는 관종 감자 (INFJ)',
+    desc: '낯가려서 잘 말하지 못하지만 사실 관심 받는게 즐거운 감자 '
+  },
+  {
+    name: '찐 감자 (ISFP)',
     desc: '감자들이 인정한 찐 감자 '
   },
   {
-    name: '그냥 <인간>',
-    desc: 'ㅇㄹㄴ '
+    name: '찐 감자 (INFP)',
+    desc: '감자들이 인정한 찐 감자 '
+  },
+  {
+    name: '찐 감자 (ESFJ)',
+    desc: '감자들이 인정한 찐 감자 '
   },
 ]
+
